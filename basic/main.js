6fde73c75767787bad3ade8a78d28bca784084dc
haru-oss <hauyaishungry@gmail.com>
2024-03-16 20:27:18 +0900

    反省！！リファクタリングする前に、プッシュしとけばよかった！

--- basic/main.js
+++ basic/main.js
@@ -1,10 +1,116 @@
 
 // 問題文
-
+const question1 = "ゲーム市場、最も売れたゲーム機は次のどれ" ;
 
 
 // 選択肢
 
+const answers = [
+    "プレステ",
+    "任天堂DS",
+    "任天堂スイッチ",
+    "ファミコン",
+];
+
+// これの長さ
+// let AnsLength = (answers.length);
 
 
 // クイズの答え
+
+const correct = "任天堂DS" ;
+
+const $button = document.getElementsByTagName("button");
+
+
+
+
+
+// let AnsLength = (answers.length);
+
+
+
+// $button[0].textContent
+// $button[1].textContent = answers[1];
+// $button[2].textContent = answers[2];
+// $button[3].textContent = answers[3];
+
+// ↑↑を注目！！！　　　　　　　　　下の処理を元々この文で行なっていた。
+
+
+// クイズの問題を選択していぎ
+const setupQuiz = () => {
+    document.getElementById("js-question").textContent = question1;
+    let buttunIndex = 0;
+    let buttunLength = $button.length;
+    while(buttunIndex < buttunLength){
+        $button[buttunIndex].textContent = answers[buttunIndex];
+        buttunIndex++}
+        }    
+        
+setupQuiz();
+
+
+// let buttunLength = $button.length
+// let buttunIndex = 0;
+// while(buttunIndex < buttunLength){
+    
+//     $button[buttunIndex].textContent = answers[buttunIndex];
+//     buttunIndex++;
+
+// }
+
+
+
+
+
+
+// ///////////////////////////////////////////////////////////////////    
+
+$button[0].addEventListener('click', () => {
+    if(correct === $button[0].textContent){
+        window.alert("正解！")
+
+    }else {
+        window.alert("不正解！")
+
+    }
+
+})
+// ///////////////////////////////////////////////////////////////////    
+$button[1].addEventListener('click', () => {
+    if(correct === $button[1].textContent){
+        window.alert("正解！")
+
+    }else {
+        window.alert("不正解！")
+
+    }
+
+})
+// ///////////////////////////////////////////////////////////////////    
+
+
+$button[2].addEventListener('click', () => {
+    if(correct === $button[2].textContent){
+        window.alert("正解！")
+
+    }else { 
+        window.alert("不正解！")
+
+    }
+
+})
+
+// ///////////////////////////////////////////////////////////////////    
+
+$button[3].addEventListener('click', () => {
+    if(correct === $button[3].textContent){
+        window.alert("正解！")
+
+    }else {
+        window.alert("不正解！")
+
+    }
+
+})
